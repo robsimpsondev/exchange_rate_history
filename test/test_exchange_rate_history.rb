dc2aa304a999e705ac2d8de516b9bc2ca32601c2
Rob Simpson <robsimpsondev@gmail.com>
2018-10-24 11:29:03 +0100

    Added initial tests

---
+++ test/test_exchange_rate_history.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'exchange_rate_history'
+
+class ExchangeRateHistoryTest < Minitest::Test
+
+  def setup
+    # nothing
+  end
+
+  def teardown
+    # nothing
+  end
+
+  def test_io_with_default_source
+    out, err = capture_io do
+      ExchangeRateHistory.puts_rate
+    end
+    assert_empty err
+    assert_equal "1.00\n", out 
+  end
+
+  def test_io_unknown_source
+    out, err = capture_io do
+      ExchangeRateHistory.puts_rate "unknown_source"
+    end
+    assert_empty err
+    assert_equal "rate source not found\n", out
+  end
+end
