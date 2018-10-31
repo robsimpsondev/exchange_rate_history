ecfca837130df2055025b63a04ebcf1b7b5b4095
Rob Simpson <robsimpsondev@gmail.com>
2018-10-31 19:07:57 +0000

    Little change to comment

--- lib/exchange_rate_history/source.rb
+++ lib/exchange_rate_history/source.rb
@@ -199,10 +199,10 @@ class ExchangeRateHistory::Source
 
 
   def get_rate_at(time_str, to_currency, from_currency = @source_counter_currency)
-    # N.B since an exchnage would never offer the inverse
+    # N.B since an exchange would never offer the inverse
     # of their selling price as their buying price then
     # calculating any rate where from_currency
-    # is not the source_counter_currency should be forbidden.
+    # is not the source_counter_currency should really be forbidden.
     if to_currency == from_currency
       return "1.00"
     elsif from_currency == @source_counter_currency
